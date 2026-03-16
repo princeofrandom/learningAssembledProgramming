bd4ada1c1db7db028f2e4bb43c66d827c85bdf95
princeofrandom <jdckennamer@gmail.com>
2026-03-16 10:34:17 -0400

    kept working on alarm clock, got input checking to work

--- Python/alarmclock.py
+++ Python/alarmclock.py
@@ -7,4 +7,15 @@ import os
 import datetime
 import math
 
+def checkalarmtime(alarmtimeinput):
+    if int(alarmtimeinput[0:2]) > 24 or int(alarmtimeinput[2:4]) > 59:
+        return False
+    else:
+        return True
+    
+
+timeset = input("What time do you want the alarm set for, in military time? ")
+while checkalarmtime(timeset) == False:
+    print("You didn't enter a valid time! Try again!")
+    timeset = input("What time do you want the alarm set for, in military time? ")
 
