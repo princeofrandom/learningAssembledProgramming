d6aec26ec6bfd006c540d689eda60444dfe2165e
princeofrandom <jdckennamer@gmail.com>
2026-03-18 15:49:44 -0400

    working on the game of life

--- Python/rplife/rplife/grid.py
+++ Python/rplife/rplife/grid.py
@@ -1,5 +1,8 @@
 import collections
 
+ALIVE = "*"
+DEAD = "."
+
 class lifeGrid:
     def __init__(self, Pattern):
         self.Pattern = Pattern
@@ -17,27 +20,34 @@ class lifeGrid:
         )
 
         num_neighbors = collections.defaultdict(int)
-        for row, col in self.pattern.alive_cells:
+        for row, col in self.Pattern.alive_cells:
             for drow, dcol in neighbors:
                 num_neighbors[(row + drow, col + dcol)] += 1
 
         stay_alive = {
             cell for cell, num in num_neighbors.items() if num in {2,3}
-        } & self.pattern.alive_cells
+        } & self.Pattern.alive_cells
         come_alive = {
             cell for cell, num in num_neighbors.items() if num == 3
-        } - self.pattern.alive_cells    
+        } - self.Pattern.alive_cells    
 
-        self.pattern.alive_cells = stay_alive | come_alive
+        self.Pattern.alive_cells = stay_alive | come_alive
 
 
     def as_string(self, bbox):
-        return (
-            f"{self.pattern.name}:\n"
-            f"Alive cells -> {sorted(self.pattern.alive_cells)}"
-        )
-        
+        start_col, start_row, end_col, end_row = bbox
+        display = [self.Pattern.name.center(2 * (end_col - start_col))]
+        for row in range(start_row, end_row):
+            display_row = [
+                ALIVE if (row, col) in self.Pattern.alive_cells else DEAD
+                for col in range(start_col, end_col)
+            ]
+            display.append("".join(display_row))
+        return "\n".join(display)
 
     def __str__(self):
-        pass
+        return (
+            f"{self.Pattern.name}:\n"
+            f"Alive cells -> {sorted(self.Pattern.alive_cells)}"
+        )
 
