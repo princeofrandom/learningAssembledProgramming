f4fcbfa2dbd39c9d5c0401ca1feca0fdc33183af
princeofrandom <jdckennamer@gmail.com>
2026-03-18 20:44:25 -0400

    continued work on game of life

--- Python/rplife/rplife/patterns.py
+++ Python/rplife/rplife/patterns.py
@@ -1,8 +1,31 @@
 from dataclasses import dataclass
+from pathlib import Path
+
+PATTERNS_FILE = Path(__file__).parent / "patterns.toml"
+
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
 
 @dataclass
 class Pattern:
     name: str # lets you make a name for each instance of a pattern object
     alive_cells: set[tuple[int, int]] # defines the initial set of starting "alive" cells as a set of two-integer tuples
 
+    @classmethod
+    def from_toml(cls, name, toml_data):
+        return cls(
+            name,
+            alive_cells = {tuple(cell) for cell in toml_data["alive_cells"]},
+        )
+    
+def get_pattern(name, filename=PATTERNS_FILE):
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return Pattern.from_toml(name, toml_data=data[name])
 
+def get_all_patterns(filename=PATTERNS_FILE):
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return [
+        Pattern.from_toml(name, toml_data) for name, toml_data in data.items()
+    ]
