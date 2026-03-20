04b3bda4a916a5748b13752f1daa998c43f6e70d
princeofrandom <jdckennamer@gmail.com>
2026-03-20 13:42:06 -0400

    day1.c isn't going anywhere! Stop deleting it!

--- C/AdventOfCode/day1.c
+++ C/AdventOfCode/day1.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     FILE *fptr;
+    char dir[1];
     char mystring[100];
-
-    char* cursor;
+    char *direction;
+    int cursor;
     int sum = 0;
 
     int dial = 50;
     fptr = fopen("d1test.txt", "r");
     // printf("%s", fgets(mystring, 100, fptr));
-    
+    printf("I started working");
     for (int i = 0; i < 1; i++)
     {
-        printf("%d\n", dial);
-        cursor = fgets(mystring, 20, fptr);
-        if (cursor[0] == 'R') {
-            cursor++;
-            printf("Cursor is now %s \n", cursor);
-            for(int i = 0; i < *cursor; i++) {
+        // printf("%d\n", dial);
+        direction = fgets(dir, 1, fptr);
+        cursor = strtol(fgets(mystring, 20, fptr),20,10);
+        if (direction == 'R') {
+            printf("Move Right\n");
+            printf("Cursor is now %d \n", cursor);
+            for(int i = 0; i < cursor; i++) {
                 dial++;
                 if (dial == 100) {
                     dial = 0;
@@ -29,11 +32,12 @@ int main()
             }
             // printf("%s", cursor);
         }
-        if (cursor[0] == 'L') {
-            cursor++;
-            printf("Cursor is now %s \n", cursor);
-            for(int i = 0; i < int() cursor; i++) {
-                printf("%d", *cursor);
+        if (direction == 'L') {
+            printf("Move Left\n");
+            // printf("Cursor is now %s \n", cursor);
+            for(int i = 0; i < cursor; i++) {
+                
+                // printf("%d", *cursor);
                 dial--;
                 if (dial == -1) {
                     dial = 99;
