ba3aeb864774cbfe2daa33a5d08a5b25aaaf93d1
princeofrandom <jdckennamer@gmail.com>
2026-03-28 13:32:18 -0400

    Started Working on First IronForgeCyber challenge for learning C.

---
+++ C/IFCChallenges/calCount.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+typedef struct cadetNode 
+{
+    int cadNum; //The cadet's serial number
+    int calsum; //the total number of calories in the cadet's pack
+    struct cadetNode* nextNode; //The pointer to the next cadet. 
+};
+
+int main() 
+{
+
+
+
+    return 0;
+}
